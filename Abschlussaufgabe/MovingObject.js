f3cb9f873f79f823ae273965114d47bf0a773160
denni <denni@DESKTOP-MEMBM47.fritz.box>
2018-02-12 11:24:10 +0100

    Abschlussaufgabe angefangen

---
+++ Abschlussaufgabe/MovingObject.js
@@ -0,0 +1,24 @@
+var aufgabe6;
+(function (aufgabe6) {
+    class MovingObjects {
+        constructor(_x, _y) {
+            this.x = _x;
+            this.y = _y;
+        }
+        ;
+        update() {
+            this.draw();
+            this.move();
+        }
+        draw() {
+            //bla
+        }
+        ;
+        move() {
+            //bla    
+        }
+        ;
+    }
+    aufgabe6.MovingObjects = MovingObjects;
+})(aufgabe6 || (aufgabe6 = {}));
+//# sourceMappingURL=MovingObject.js.map
---
+++ Abschlussaufgabe/main.ts
@@ -0,0 +1,72 @@
+//Aufgabe: (Aufgabe 1)
+//Name: (Marcel Hasemann)
+//Matrikel: (256646)
+//Datum: (12.10.2017)
+//Hiermit versichere ich, dass ich diesen
+//Code selbst geschrieben habe. Er wurde
+//nicht kopiert und auch nicht diktiert.
+namespace aufgabe6 {
+    window.addEventListener("load", init);
+    export let crc2: CanvasRenderingContext2D;
+    let img: ImageData;
+
+    let object: MovingObjects[] = [];
+
+    function init(): void {
+        let canvas: HTMLCanvasElement = document.getElementsByTagName("canvas")[0];
+
+        crc2 = canvas.getContext("2d");
+        let n: number = 5;
+
+        //Himmel
+        crc2.fillStyle = "#5CACEE";
+        crc2.fillRect(0, 0, 800, 600);
+
+
+
+        //Sonne
+        crc2.beginPath();
+        crc2.arc(695, 60, 40, 0, 2 * Math.PI);
+        crc2.fillStyle = "#FFD700";
+        crc2.fill();
+
+
+
+
+
+        for (let i: number = 0; i < n; i++) {
+            let s: Ski = new Ski(0, 0, "#0000ff", Math.random() * 2 + 5, Math.random() * 2 + 5);
+            object.push(s);
+            let t: Baum = new Baum(50 + Math.random() * 170, 200 + Math.random() * 150);
+            object.push(t);
+            let w: Wolken = new Wolken(0 + Math.random() * 800, 0 + Math.random() * 80);
+            object.push(w);
+
+        }
+        let m: number = 150;
+        for (let i: number = 0; i < m; i++) {
+            let sn: Snow = new Snow(0 + Math.random() * 800, 0 + Math.random() * 600);
+            object.push(sn);
+
+        }
+        img = crc2.getImageData(0, 0, 800, 600);
+        console.log(img);
+        animate();
+    }
+
+    //Animate 
+    function animate(): void {
+        crc2.putImageData(img, 0, 0);
+
+        for (let i: number = 0; i < object.length; i++) {
+            let s: MovingObjects = object[i];
+            s.update();
+        }
+
+
+        window.setTimeout(animate, 20);
+    }
+
+
+
+}
